0432a31c9f34b2b1385e154806aaeea746198524
larutyunov <larutyunov@diasoft.ru>
2022-06-22 23:44:49 +0300

    fix db script and entity ProjectRole

--- src/main/java/com/devlife/pf_sql_controller/entity/ProjectRole.java
+++ src/main/java/com/devlife/pf_sql_controller/entity/ProjectRole.java
@@ -14,19 +14,26 @@ public class ProjectRole {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "role_id")
-    private Long roleId;
-
-    @Column(name = "project_id")
-    private Long projectId;
-
-//    @ManyToOne
-//    @JoinColumn(name = "project_id")
-//    Project project;
+//    @Column(name = "role_id")
+//    private Long roleId;
 //
-//    @ManyToOne
-//    @JoinColumn(name = "role_id")
-//    Role role;
+//    @Column(name = "user_id")
+//    private Long userId;
+//
+//    @Column(name = "project_id")
+//    private Long projectId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    Role role;
 
     @Column(name = "date", columnDefinition = "DATE")
     private LocalDate date;
